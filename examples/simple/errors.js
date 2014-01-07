300144314fcfcbe2b047d633f56a33015becc1f1
MaxGfeller <max.gfeller@gmail.com>
2014-01-07 22:25:16 +0100

    Added an example on how errors can be used

---
+++ examples/simple/errors.js
@@ -0,0 +1,32 @@
+var wflow = require('../../index.js');
+
+var workflowDefinition = [
+	{ 'sequence': [
+		function(next) {
+			console.log('Step 1');
+			this.answer = 43;
+			next();
+		},
+		function(next) {
+			console.log('Step 2');
+			if(this.answer !== 42) {
+				return next(new Error('Answer is wrong!'))
+			}
+			next();
+		},
+		function(next) {
+			console.log('Am i being called?');
+			next();
+		}
+	] }
+];
+
+wflow.setDefinition(workflowDefinition);
+
+wflow.run(null, function() {
+	console.log('Finished!')
+});
+
+wflow.on('error', function(err) {
+	console.log('Error: ' + err.message);
+});
